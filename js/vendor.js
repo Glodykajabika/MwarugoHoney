7ed620e133db82e2b96df4435d3c5e595f871846
Glody Kajabika <kajabikaglody8@gmail.com>
2025-06-03 16:45:42 +0800

    Modifications of the sticky nav func

--- js/vendor.js
+++ js/vendor.js
@@ -30,16 +30,20 @@ activeLink();
 
 // Sticky nav 
 const navbar = document.querySelector('.nav');
+const header__navbar = document.getElementById('header__navbar');
 
 window.addEventListener('scroll', function () {
 
-    const viewportHeight = window.innerHeight * .9;
+    const viewportHeight = window.innerHeight * .5;
 
     if (window.scrollY >= viewportHeight) {
         navbar.classList.add('nav--sticky');
-
+        header__navbar.style.position = 'fixed';
+        
     } else {
         navbar.classList.remove('nav--sticky');
+        header__navbar.style.position = 'relative';
+
     }
 
 });
