739def537e001f8130167eff6f48005f0eff2386
Glody Kajabika <kajabikaglody8@gmail.com>
2025-08-28 18:15:05 +0800

    Hamburger

--- js/vendor.js
+++ js/vendor.js
@@ -2,11 +2,15 @@
 // Sticky nav 
 const navbar = document.querySelector('.nav');
 const header__navbar = document.getElementById('header__navbar');
+const hamburger = document.querySelector('.hamburger');
+const header__linksBox = document.querySelector(".header__links-box");
 
+
+// Sticky navigation bar
 window.addEventListener('scroll', function () {
-
-    const viewportHeight = window.innerHeight * .99;
-
+    
+    const viewportHeight = window.innerHeight * .59;
+    
     if (window.scrollY >= viewportHeight) {
         navbar.classList.add('nav--sticky');
         header__navbar.style.position = 'fixed';
@@ -16,5 +20,12 @@ window.addEventListener('scroll', function () {
         header__navbar.style.position = 'relative';
 
     }
+    
+});
 
+
+// Hamburger menu toggle
+hamburger.addEventListener('click', () => {
+    header__linksBox.classList.toggle('active');
+    hamburger.classList.toggle('active');
 });
